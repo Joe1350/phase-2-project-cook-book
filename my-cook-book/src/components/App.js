94f31a5a685c63f3c7e6cc863f0512d982f83442
Joseph Martin <josephmartin1350@gmail.com>
2022-02-04 10:54:07 -0700

    added fetch and started RecipiesList

--- my-cook-book/src/components/App.js
+++ my-cook-book/src/components/App.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [recipies, setRecipies] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/recpies")
+    .then(r => r.json())
+    .then(data => setRecipies(data.recipies))
+  }, [])
+  
   return (
     <div>
       <h1>placeholder</h1>
---
+++ my-cook-book/src/components/RecipiesList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RecipiesList({ recipies }) {
+    return (
+        {recipies.name}
+    )
+}
