00b9703a872c8f443d9e017b0e0773a303b25f4f
Joseph Martin <josephmartin1350@gmail.com>
2022-02-04 11:39:12 -0700

    added HomePage and RecipieForm components

---
+++ my-cook-book/src/components/RecipieForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RecipieForm() {
+    return (
+        <form>
+            <input type="text" name="placeholder" id="placeholder" value={""} onChange={null} />
+        </form>
+    )
+}
+
+export default RecipieForm;
