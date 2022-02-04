e02532bc2276679ff1d78c4e67eca682065542c7
Joseph Martin <josephmartin1350@gmail.com>
2022-02-04 13:52:12 -0700

    added RecipieDetails component

---
+++ my-cook-book/src/components/RecipieDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function RecipieDetails() {
+    // this will show the ingredients and description when RecipieListing is clicked
+    return (
+        <p>placeholder</p>
+    )
+}
+
+export default RecipieDetails;
