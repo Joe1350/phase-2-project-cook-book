4b9a8f25df74e5405e08794342fb429b38307b17
Joseph Martin <josephmartin1350@gmail.com>
2022-02-05 13:48:55 -0700

    added a recipie, fixed syntax error in css, edited notes

--- my-cook-book/src/components/App.js
+++ my-cook-book/src/components/App.js
@@ -7,20 +7,24 @@ import RecipiesList from "./RecipiesList";
 import RecipieForm from "./RecipieForm";
 
 // things to do
-  // maybe useContext on the form, and maybe the recipies... probably not, it depends on how RecipieDetails goes.
   // break the form up into components
+    // maybe useContext on the form, depends on how component breakup goes
   // get RecipieDetails component written and set up
     // making onClick work in recipieListings
     // figure out the route and path change
+    // and maybe the recipies... probably not, it depends on how RecipieDetails goes.
   // make the document title dynamic with a custom hook
-  // enter at least one more recipies
+  // when you click add recipie, redirect to new RecipieDetails
   // set styles
+
 // might not happen right now
   // add a category feature to the recipie list to filter by category, maybe a sort as well
   // gonna need a patch request for notes
+
 // probably not going to happen right now
   // I'd like to turn the navbar into a square with three lines that opens the navbar
   // maybe add a (pseudo)login feature
+
 // not gonna happen right now
   // duplicate recipies
   // edit recipies
