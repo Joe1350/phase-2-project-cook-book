a75ee5dedba3a62b5d84372e62847190bbf80d12
Joseph Martin <josephmartin1350@gmail.com>
2022-02-05 00:34:15 -0700

    fixed header link

--- my-cook-book/src/components/Header.js
+++ my-cook-book/src/components/Header.js
@@ -2,9 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        // I'd like to add an anchor tag to direct to the home page
-        // I'd like to make it work correctly
-        <h1><a href="#home">My Cook Book</a></h1>
+        <h1><a href="/">My Cook Book</a></h1>
     )
 }
 
