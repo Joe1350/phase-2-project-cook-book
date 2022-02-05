d840c8541429265c50ab0d5d0d66651e91560d8d
Joseph Martin <josephmartin1350@gmail.com>
2022-02-05 00:29:30 -0700

    added and edited notes

--- my-cook-book/src/components/App.js
+++ my-cook-book/src/components/App.js
@@ -7,19 +7,21 @@ import RecipiesList from "./RecipiesList";
 import RecipieForm from "./RecipieForm";
 
 // things to do
-// maybe useContext on the form
+// maybe useContext on the form, and maybe the recipies... probably not, it depends on how RecipieDetails goes.
 // break the form up into components
 // get RecipieDetails component written and set up
   // making onClick work in recipieListings
   // figure out the route and path change
 // make the document title dynamic with a custom hook
-// write home page
-// enter a couple more recipies
+// enter at least one more recipies
 // set styles
 // I'd like to turn the navbar into a square with three lines that opens the navbar
 // maybe add a (pseudo)login feature
 // add a category feature to the recipie list to filter by category, maybe a sort as well
 // gonna need a patch request for notes
+// not gonna happen right now
+  // duplicate recipies
+  // edit recipies
 
 function App() {
   const [recipies, setRecipies] = useState([])
