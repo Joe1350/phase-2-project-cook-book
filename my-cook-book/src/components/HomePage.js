bce609bcdb8a16e69c2d3d54f47aea5b901c70b1
Joseph Martin <josephmartin1350@gmail.com>
2022-02-05 00:26:06 -0700

    worked on homepage

--- my-cook-book/src/components/HomePage.js
+++ my-cook-book/src/components/HomePage.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function HomePage() {
+    // 
     return (
-        <h1>This is my homepage</h1>
+        <div style={{ width: "80%", textAlign: "center", marginLeft: "5%", marginTop: "25px"}}>
+            <img style={{ width: "50%" }} src="https://www.lanascooking.com/wp-content/uploads/2014/01/spanish-rice-and-chicken-soup-feature.jpg"/>
+            <p>
+                Thank you for checking out My Cook Book web application. This website is a list of my favorite recipies and recipies that I want to try. 
+            </p>
+        </div>
+        
     )
 }
 
