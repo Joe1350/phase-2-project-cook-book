457191614619e9d5945b0a459ec734cc41263d5a
Joseph Martin <josephmartin1350@gmail.com>
2022-02-06 14:02:16 -0700

    edited notes

--- my-cook-book/src/components/App.js
+++ my-cook-book/src/components/App.js
@@ -39,13 +39,36 @@ import RecipieForm from "./RecipieForm";
   // add a Footer component
   // duplicate recipies
   // edit recipies
-    // maybe any edit makes a duplicate, and then you can delete previous versions later later
+    // maybe any edit makes a duplicate, and then you can delete previous versions later
       // (or, be asked if you want to delete previous version or keep previous version)
-      // also, you could use navlinks and routes to display the different versions
+      // also, you could use navlinks and routes to display the different versions in RecipieDetails
   // delete recipies
   // add a createdAt key in the db
   // maybe add a (pseudo)login feature
-  // add a favorite recipies "✰" button(grab star from GregsList(mock code challenge))
+  // add a favorite recipies and implement logic
+    // {true ? (
+    //   <button className="emoji-button favorite active">★</button>
+    // ) : (
+    //   <button className="emoji-button favorite">☆</button>
+    // )}
+
+    // .emoji-button {
+    //     border: none;
+    //     background: none;
+    //     padding: 0 0.25rem;
+    //     cursor: pointer;
+    //   }
+
+    //   .emoji-button.favorite {
+    //     color: transparent;
+    //     text-shadow: 0 0 0 gray;
+    //   }
+
+    //   .emoji-button.favorite.active,
+    //   .emoji-button.favorite:hover {
+    //     text-shadow: 0 0 0 gold;
+    //     cursor: pointer;
+    //   }
   // I'd like to turn the navbar into a square with three lines that opens the navbar and animates to an x
   // add RecipieNotes component
   // add an area to display notes in state, and a form to add new notes
